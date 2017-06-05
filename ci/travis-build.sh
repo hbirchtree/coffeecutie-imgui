1cb5fd4f0a1f8103488252bde2112a84ea43f10d
Håvard Bjerke <h0wy36@gmail.com>
2017-06-05 19:24:48 +0200

    - Fix build status on failure

--- ci/travis-build.sh
+++ ci/travis-build.sh
@@ -16,6 +16,13 @@ function build_standalone()
         -e SOURCE_DIR="$SOURCE_DIR" \
         -e COFFEE_DIR="$COFFEE_DIR" $@ \
         -e EXTRA_OPTIONS=""
+
+    # We want to exit if the Make process fails horribly
+    # Should also signify to Travis/CI that something went wrong
+    EXIT_STAT=$?
+    if [[ ! "$EXIT_STAT" = 0 ]]; then
+        exit 1
+    fi
 }
 
 case "${TRAVIS_OS_NAME}" in
