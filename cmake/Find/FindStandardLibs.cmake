848df3107495f39ffc1ab538c312fd55f9997fa2
Håvard Bjerke <h0wy36@gmail.com>
2018-02-25 17:03:39 +0100

    - New engine build

--- cmake/Find/FindStandardLibs.cmake
+++ cmake/Find/FindStandardLibs.cmake
@@ -93,7 +93,7 @@ if(ANDROID)
     if(COFFEE_BUILD_SDL2)
         list ( APPEND CORE_EXTRA_LIBRARIES
             SDL2
-            ${SDL2_LIBRARIES}
+#            ${SDL2_LIBRARIES}
             )
     endif()
 
@@ -147,6 +147,10 @@ if(WIN32)
             OneCore
             )
     endif()
+
+    list ( APPEND CORE_EXTRA_LIBRARIES
+        Cabinet
+        )
 
     if(MINGW64)
         list ( APPEND CORE_EXTRA_LIBRARIES
