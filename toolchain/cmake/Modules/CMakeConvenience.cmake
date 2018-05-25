930b1e155ab8f6989e6ac5a114767ba49071504a
Håvard Bjerke <h0wy36@gmail.com>
2018-05-25 16:02:21 +0200

    - Update to new project structure

---
+++ toolchain/cmake/Modules/CMakeConvenience.cmake
@@ -0,0 +1,9 @@
+macro(TARGET_ENABLE_CXX11 TARGET)
+    if(NOT ANDROID AND NOT APPLE)
+        target_compile_features(${TARGET} PRIVATE cxx_constexpr)
+    endif()
+
+    if(EMSCRIPTEN)
+        target_compile_features(${TARGET} PRIVATE cxx_relaxed_constexpr )
+    endif()
+endmacro()
